fae5cb655f6035193344718edc3f875754c3dd9f
Chadi7781 <troudishedy6@gmail.com>
2022-03-14 12:13:22 +0100

    final

---
+++ InvoiceManagementApp/InvoiceManagementApp.Api/ClientApp/src/components/invoices/Invoices.tsx
@@ -0,0 +1,40 @@
+﻿import * as React from 'react';
+
+import { useState, useEffect } from 'react';
+import { InvoiceVm, InvoicesClient } from '../../utils/api';
+import { getBalance } from '../../utils/invoiceUtils';
+
+interface IInvoices {
+
+}
+
+const Invoices: React.FC<IInvoices> = ({ }) => {
+    const [invoices, setInvoices] = useState<InvoiceVm[]>([]);
+    const client = new InvoicesClient();
+
+    useEffect(() => {
+        client.get().then(res => setInvoices(res))
+            .catch(err => console.log(err));
+    }, [])
+
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Invoice #</th>
+                        <th>Balance</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {invoices && invoices.map(invoice => <tr key={`invoice-${invoice.id}`}>
+                        <td>{invoice.invoiceNumber}</td>
+                        <td>{getBalance(invoice)}</td>
+                    </tr>)}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Invoices
